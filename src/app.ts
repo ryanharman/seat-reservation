630961b7b98bb34a7c44970e5cca12057df0e632
Ryan Harman <ryan.harman63@gmail.com>
2021-04-05 16:15:37 +0100

    cloned ts-express

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,66 @@
 import express, { Application, Request, Response, NextFunction } from "express";
+import http from "http";
+import logging from "./config/logging";
+import config from "./config/config";
+import sampleRoutes from "./routes/sample";
 
+const NAMESPACE = "Server";
 const app: Application = express();
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello");
+// Logging the request
+app.use((req: Request, res: Response, next: NextFunction) => {
+  logging.info(
+    NAMESPACE,
+    `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]`
+  );
+
+  res.on("finish", () => {
+    logging.info(
+      NAMESPACE,
+      `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}], STATUS - [${res.statusCode}]`
+    );
+  });
+
+  next();
 });
 
-app.listen(5000, () => console.log("Server is running"));
+// Parse the request
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// API rules
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*"); // remove for prod
+  res.header(
+    "Access-Control-Allow-Header",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  if (req.method == "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST PUT");
+    return res.status(200).json({});
+  }
+
+  next();
+});
+
+// Routes
+app.use("/sample", sampleRoutes);
+
+// Error handling
+app.use((req: Request, res: Response) => {
+  const error = new Error("not found");
+
+  return res.status(404).json({
+    message: error.message,
+  });
+});
+
+// Create the server
+const httpServer = http.createServer(app);
+httpServer.listen(config.server.port, () => {
+  logging.info(
+    NAMESPACE,
+    `Server running on ${config.server.hostname}:${config.server.port}`
+  );
+});
