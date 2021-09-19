575774aa5fe9b5e998073cbd6b0a4834421b41e7
Ryan Harman <ryan.harman63@gmail.com>
2021-09-19 19:46:40 +0100

    modal fix hallelujah. still got the actions to fix tho

--- seat-res-app/components/ui/Modal.tsx
+++ seat-res-app/components/ui/Modal.tsx
@@ -14,31 +14,49 @@ const Modal = ({ useModalStore }: ModalProps) => {
   return (
     <>
       {isOpen && (
-        <div className="fixed flex justify-center items-center bg-black bg-opacity-50 w-screen h-screen z-30">
-          <Card className="w-96 z-50">
-            <CardHeader className="mb-8">
-              <div>{title}</div>
-              <Icon
-                icon="expand"
-                className="cursor-pointer hover:text-blue-500"
-                width={20}
-                height={20}
-                onClick={() => onClose(data)}
-              />
-            </CardHeader>
-            {content}
-            <div className="flex items-center justify-end gap-4 mt-8">
-              <Button className="text-sm" primary={false} onClick={() => onCancel(data)}>
-                {cancelText}
-              </Button>
-              <Button className="text-sm" primary onClick={() => onConfirm(data)}>
-                {confirmText}
-              </Button>
-            </div>
-          </Card>
-        </div>
+        <Backdrop onClick={onClose}>
+          <div onClick={(e) => e.stopPropagation()} className="z-50">
+            <Card className="w-96">
+              <CardHeader className="mb-8">
+                <div>{title}</div>
+                <Icon
+                  icon="expand"
+                  className="cursor-pointer hover:text-blue-500"
+                  width={20}
+                  height={20}
+                  onClick={() => onClose(data)}
+                />
+              </CardHeader>
+              {content}
+              <div className="flex items-center justify-end gap-4 mt-8">
+                <Button className="text-sm" primary={false} onClick={() => onCancel(data)}>
+                  {cancelText}
+                </Button>
+                <Button className="text-sm" primary onClick={() => onConfirm(data)}>
+                  {confirmText}
+                </Button>
+              </div>
+            </Card>
+          </div>
+        </Backdrop>
       )}
     </>
+  );
+};
+
+interface BackdropProps {
+  onClick: () => void;
+  children: React.ReactChild[] | React.ReactChild;
+}
+
+const Backdrop = ({ onClick, children }: BackdropProps) => {
+  return (
+    <div
+      className="fixed flex justify-center items-center bg-black bg-opacity-50 w-screen h-screen z-30"
+      onClick={() => onClick()}
+    >
+      {children}
+    </div>
   );
 };
 
